85d51a68741cfa594529a3c912e4a086766b8162
Victor Soares <victor.soares54327@gmail.com>
2026-04-26 11:54:34 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+const dataDoEvento = new Date("may 21, 2026 19:00:00");
+const timeStampEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval( function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diasEmMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diasEmMs) / horasEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horasEmMs) / minutosEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
+
+    if(distanciaAteEvento < 0 ) {
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = 'evento expirado'
+    }
+
+}, 1000);
