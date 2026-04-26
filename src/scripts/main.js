02f6017fdd80113aa419375e4e53f26d5c784ed8
Victor Soares <victor.soares54327@gmail.com>
2026-04-26 12:18:58 -0300

    alterando a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("may 21, 2026 19:00:00");
+const dataDoEvento = new Date("jun 6, 2026 19:00:00");
 const timeStampEvento = dataDoEvento.getTime();
 
 const contaHoras = setInterval( function() {
